66e3ab5a3dd58b29b9c782f5f62565cdb27e36c1
Esperanza <v.esperanzac113@gmail.com>
2022-10-13 01:43:22 -0700

    Add detail page

---
+++ RecipeListApp/Views/RecipeDetailView.swift
@@ -0,0 +1,63 @@
+//
+//  RecipeDetailView.swift
+//  RecipeListApp
+//
+//  Created by Esperanza on 2022-10-13.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+  
+  var recipe: Recipe
+  var body: some View {
+    ScrollView {
+      VStack(alignment: .leading) {
+        // MARK: Recipe Image
+        Image(recipe.image)
+          .resizable()
+          .scaledToFill()
+        
+        // MARK: Recipe Ingredients
+        VStack(alignment: .leading) {
+          Text("Ingredients")
+            .font(.headline)
+            .padding([.bottom, .top], 5)
+          ForEach(recipe.ingredients, id: \.self) { item in
+            Text("• " + item)
+          }
+        }
+        .padding(.horizontal)
+        
+        // MARK: Divider
+        
+        // MARK: Recipe Directions
+        VStack(alignment: .leading) {
+          Text("Directions")
+            .font(.headline)
+            .padding([.bottom, .top], 5)
+          ForEach(0..<recipe.directions.count - 1, id: \.self) {
+            index in
+            Text(String(index + 1) + ". " + recipe.directions[index])
+              .padding(.bottom, 5)
+          }
+        }
+        .padding(.horizontal)
+        
+      }
+        
+    }.navigationBarTitle(recipe.name)
+      
+  }
+}
+
+struct RecipeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+      
+      // Create a dummy recipe and pass it into the detail view so that we can see a preview
+      
+      let model = RecipeModel()
+      
+      RecipeDetailView(recipe: model.recipes[0])
+    }
+}
--- RecipeListApp/Views/RecipeListView.swift
+++ RecipeListApp/Views/RecipeListView.swift
@@ -13,18 +13,22 @@ struct RecipeListView: View {
   @ObservedObject var model = RecipeModel()
   
     var body: some View {
-    
-      List(model.recipes) { r in
-        HStack(spacing: 20.0) {
-          Image(r.image)
-            .resizable()
-            .scaledToFill()
-            .frame(width: 50, height: 50, alignment: .center)
-            .clipped()
-            .cornerRadius(5)
-          Text(r.name)
-        }
-
+      NavigationView {
+        List(model.recipes) { r in
+          NavigationLink {
+            RecipeDetailView(recipe: r)
+          } label: {
+            HStack(spacing: 20.0) {
+              Image(r.image)
+                .resizable()
+                .scaledToFill()
+                .frame(width: 50, height: 50, alignment: .center)
+                .clipped()
+                .cornerRadius(5)
+              Text(r.name)
+            }
+          }
+        }.navigationTitle("All Recipes")
       }
     }
 }
